a91666237a9f7482fb77b876bcafee1e054beb93
Alex Kyllo <alex.kyllo@gmail.com>
2020-10-11 22:46:52 -0700

    add conda config

--- personal/config.el
+++ personal/config.el
@@ -1,5 +1,5 @@
 ;; additional packages
-(prelude-require-packages '(ess julia-mode julia-snail flycheck-julia neotree))
+(prelude-require-packages '(ess julia-mode julia-snail flycheck-julia neotree conda blacken))
 
 ;; neotree icons setup
 (global-set-key [f8] 'neotree-toggle)
@@ -7,3 +7,17 @@
 
 ;; keybindings
 (define-key ivy-minibuffer-map (kbd "RET") 'ivy-alt-done)
+
+;; conda
+(require 'conda)
+;; set miniconda home
+(custom-set-variables
+ '(conda-anaconda-home "~/miniconda3/"))
+
+(setq conda-env-home-directory (expand-file-name "~/miniconda3/"))
+;; if you want interactive shell support, include:
+(conda-env-initialize-interactive-shells)
+;; if you want eshell support, include:
+(conda-env-initialize-eshell)
+;; if you want auto-activation (see below for details), include:
+(conda-env-autoactivate-mode t)
